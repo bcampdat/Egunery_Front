fc40e012abd9f29778ef5417b1b0647c5f30c7da
Bea <bcampdat@gmail.com>
2024-09-23 18:53:26 +0200

    solo prueba

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  safelist: [
+    'order-last',
+    'lg:order-none',
+  ],
   theme: {
     extend: {
       keyframes: {
