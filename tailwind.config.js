df4ac479ae8b95ad3ac5a8f882ddc0986d6be81b
Bea <bcampdat@gmail.com>
2024-09-23 18:57:30 +0200

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  safelist: [
-    'order-last',
-    'lg:order-none',
-  ],
   theme: {
     extend: {
       keyframes: {
