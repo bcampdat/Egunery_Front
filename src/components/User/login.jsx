93aad839ab0c7eb18bb86fd653f8d8cd193fd774
Bea <bcampdat@gmail.com>
2024-09-25 20:44:23 +0200

    correcion error en ajuste login

--- src/components/User/login.jsx
+++ src/components/User/login.jsx
@@ -12,9 +12,9 @@ const Login = () => {
 
   const handleLogin = (event) => {
     event.preventDefault();
-      
+
     axios
-      .post("http://localhost:3001/api/users/login", {
+      .post(`${import.meta.env.VITE_BACKEND_URL}/api/users/login`, {
         username: username,
         password: password,
       })
@@ -55,7 +55,7 @@ const Login = () => {
       {error && <div className="text-red-500 mb-4">{error}</div>}
       {!user ? (
         <form className="flex flex-col" onSubmit={handleLogin}>
-          <CgUserlane className="lg:w-16 lg:h-16 mx-auto text-center text-gray-500" />
+          <CgUserlane className="w-10 h-10 md:w-16 md:h-16 mx-auto text-center text-gray-700 mb-4" />
           <div className="mb-4">
             <label
               className="block text-gray-700 text-sm font-bold mb-2"
@@ -68,7 +68,7 @@ const Login = () => {
               id="username"
               value={username}
               onChange={(e) => setUsername(e.target.value)}
-              className="p-2 pl-10 text-sm text-gray-700 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600"
+              className="appearance-none bg-white p-2 text-sm text-gray-700 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600"
             />
           </div>
           <div className="mb-4">
@@ -83,13 +83,13 @@ const Login = () => {
               id="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
-              className="p-2 pl-10 text-sm text-gray-700 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600"
+              className="appearance-none bg-white p-2 text-sm text-gray-700 rounded-lg border border-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-600"
               required
             />
           </div>
           <button
             type="submit"
-            className="bg-blue-600 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg mt-4"
+            className="bg-sky-400 hover:bg-sky-300 text-white font-bold py-2 px-4 rounded-lg mt-4"
           >
             Iniciar sesión
           </button>
